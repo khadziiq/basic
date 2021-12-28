91e3ca6929fb4297214386039829ff5a5d02f00c
khoirulkahf <fahrulista@gmail.com>
2021-12-28 14:04:53 +0700

    edit index

---
+++ algoritma-pseudocode/index.js
@@ -0,0 +1,33 @@
+// buat algoritma dan pseudocode buat luas persegi panjang
+// algoritma luas persegi panjang = panjang x lebar
+
+// pseudocode
+//store panjang with any number kecuali yang sudah ketetapan misal phi
+//store lebar with any number kecuali yang sudah ketetapan misal phi
+//store luas with panjang multiply lebar
+//display luas
+
+// let panjang = 10;
+// let lebar = 5;
+
+// let luas = panjang * lebar;
+// console.log(luas);
+
+//2. volume bola lalu temukan algoritma dan pseudocode
+//- cari definisi dari rumus volume bola = 4/3 *phi*r*r*r
+//- buat variabel volume, phi dan r
+//rumus
+//tampilkan volume
+
+//pseudocode
+//store r with any number
+//store phi with 3.14
+//store volume with 4/3 mul pi mul r mul r mul r
+//display volume
+
+let r = 12;
+const phi = 3.14;
+let volume_bola = 4 / 3 * phi * r * r * r;
+console.log(volume_bola);
+
+
